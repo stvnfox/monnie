f235af341690fc5ce3f4fbc9e9f5218f07284631
steven.vos <steven.vos@iodigital.com>
2024-10-16 12:52:34 +0200

    refactor: seperate index route and component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,35 +1,7 @@
-import {
-  SignedIn,
-  UserButton,
-  SignOutButton,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-} from "@clerk/tanstack-start";
 import { createFileRoute } from "@tanstack/react-router";
 
+import { Dashboard } from "../components/dashboard";
+
 export const Route = createFileRoute("/")({
-  component: Home,
+  component: Dashboard,
 });
-
-function Home() {
-  return (
-    <div>
-      <h1>Index Route</h1>
-      <SignedIn>
-        <p>You are signed in</p>
-
-        <UserButton />
-
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <p>You are signed out</p>
-
-        <SignInButton />
-
-        <SignUpButton />
-      </SignedOut>
-    </div>
-  );
-}
