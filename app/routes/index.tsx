293db835914cf1213a8ac9f6265177d6e48b8368
steven.vos <steven.vos@iodigital.com>
2024-10-28 11:32:14 +0100

    feat: check if user is logged-in before load o/w redirect to sign-in page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,25 +1,33 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
-import { SignedIn, SignedOut } from "@clerk/tanstack-start";
-import { LoggedOut } from "~/components/auth/logged-out";
 import { Dashboard } from "~/components/dashboard/dashboard";
 import { NavigationComponent } from "~/components/navigation/navigation";
+import { createServerFn } from "@tanstack/start";
+import { getAuth } from "@clerk/tanstack-start/server";
+
+const authStateFn = createServerFn("GET", async (_, { request }) => {
+  const { userId } = await getAuth(request);
+
+  if (!userId) {
+    throw redirect({
+      to: "/auth/sign-in/$",
+    });
+  }
+
+  return { userId };
+});
 
 export const Route = createFileRoute("/")({
   component: AppWrapper,
+  beforeLoad: async () => await authStateFn(),
 });
 
 function AppWrapper() {
   return (
     <section className="min-h-screen w-screen">
       <div className="container">
-        <SignedIn>
-          <NavigationComponent />
-          <Dashboard />
-        </SignedIn>
-        <SignedOut>
-          <LoggedOut />
-        </SignedOut>
+        <NavigationComponent />
+        <Dashboard />
       </div>
     </section>
   );
